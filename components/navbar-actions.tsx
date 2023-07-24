bb49e7bcca8ee2d46b878cabe39cb2ff0dbdabaa
Krishan Ramdin <Krishanramdin_@hotmail.com>
2023-07-24 22:13:49 +0200

    Push Changes Upstream

--- components/navbar-actions.tsx
+++ components/navbar-actions.tsx
@@ -1,11 +1,27 @@
 "use client";
 
+import { ShoppingBag } from "lucide-react";
+
 import Button from "@/components/ui/button";
+import { useEffect, useState } from "react";
 
 const NavbarActions = () => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) {
+    return null;
+  }
+
   return (
     <div className="ml-auto flex items-center gap-x-4">
-      <Button></Button>
+      <Button className="flex items-center rounded-full bg-black px-4 py-2">
+        <ShoppingBag color="white" size={20} />
+        <span className="ml-2 text-sm font-medium text-white">0</span>
+      </Button>
     </div>
   );
 };
--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -1,11 +1,35 @@
 import { forwardRef } from "react";
 
+import { cn } from "@/lib/utils";
+
 export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement> {}
 
 const Button = forwardRef<HTMLButtonElement, ButtonProps>(
   ({ className, children, disabled, type = "button", ...props }, ref) => {
-    return <button ref={ref}>{children}</button>;
+    return (
+      <button
+        ref={ref}
+        className={cn(
+          `
+        w-auto
+        rounded-full
+        bg-black 
+        border-transparent
+        px-5
+        py-3
+        disabled:cursor-not-allowed
+        disabled:opacity-50
+        text-white
+        font-semibold
+        hover:opacity-75transition
+        `,
+          className
+        )}
+      >
+        {children}
+      </button>
+    );
   }
 );
 
