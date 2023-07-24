bdc0de1234a506a5ed141927c6c89e4952c03dd7
Krishan Ramdin <Krishanramdin_@hotmail.com>
2023-07-24 21:24:51 +0200

    Push Changes Upstream

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,8 +1,10 @@
 import Container from "@/components/ui/container";
 import Link from "next/link";
 import MainNav from "@/components/main-nav";
+import getCategories from "@/actions/get-categories";
 
-const Navbar = () => {
+const Navbar = async () => {
+  const categories = await getCategories();
   return (
     <div className="border-b">
       <Container>
@@ -16,7 +18,7 @@ const Navbar = () => {
           <Link href="/" className="ml-4 flex lg:ml-0 gap-x-2">
             <p className="font-bold text-xl text-white">STORE</p>
           </Link>
-          <MainNav data={[]} />
+          <MainNav data={categories} />
         </div>
       </Container>
     </div>
