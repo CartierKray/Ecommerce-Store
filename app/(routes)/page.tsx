a98e756b878ee38c83bff0d8227bb59450a5fd13
Krishan Ramdin <Krishanramdin_@hotmail.com>
2023-07-24 22:34:59 +0200

    Push Changes Upstream

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,5 +1,18 @@
-const HomePage = () => {
-  return <div>Hello Store</div>;
+import getBillboard from "@/actions/get-billboard";
+import Billboard from "@/components/billboard";
+import Container from "@/components/ui/container";
+
+export const revalidate = 0;
+
+const HomePage = async () => {
+  const billboard = await getBillboard("1fb57f32-f4e6-4946-b469-fb5473ce1dba");
+  return (
+    <Container>
+      <div className="space-y-10 pb-10">
+        <Billboard data={billboard} />
+      </div>
+    </Container>
+  );
 };
 
 export default HomePage;
