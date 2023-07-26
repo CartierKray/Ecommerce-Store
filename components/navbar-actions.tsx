06dfc32b2ef45da03bd68171cb0218236b5f4f23
Krishan Ramdin <Krishanramdin_@hotmail.com>
2023-07-26 22:41:58 +0200

    Push Changes Upstream

--- components/navbar-actions.tsx
+++ components/navbar-actions.tsx
@@ -1,9 +1,10 @@
 "use client";
 
 import { ShoppingBag } from "lucide-react";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
 
 import Button from "@/components/ui/button";
-import { useEffect, useState } from "react";
 import useCart from "@/hooks/use-cart";
 
 const NavbarActions = () => {
@@ -13,6 +14,8 @@ const NavbarActions = () => {
     setIsMounted(true);
   }, []);
 
+  const router = useRouter();
+
   const cart = useCart();
 
   if (!isMounted) {
@@ -21,7 +24,10 @@ const NavbarActions = () => {
 
   return (
     <div className="ml-auto flex items-center gap-x-4">
-      <Button className="flex items-center rounded-full bg-black px-4 py-2">
+      <Button
+        onClick={() => router.push("/cart")}
+        className="flex items-center rounded-full bg-black px-4 py-2"
+      >
         <ShoppingBag color="white" size={20} />
         <span className="ml-2 text-sm font-medium text-white">
           {cart.items.length}
