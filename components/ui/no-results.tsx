e63e98e7ce21d4ec7d881eefcf9bb801221c64c3
Krishan Ramdin <Krishanramdin_@hotmail.com>
2023-07-26 10:42:38 +0200

    Push Changes Upstream

---
+++ components/ui/no-results.tsx
@@ -0,0 +1,9 @@
+const NoResults = () => {
+  return (
+    <div className="flex items-center justif-center h-full w-full text-neutral-500">
+      No results were found.
+    </div>
+  );
+};
+
+export default NoResults;
